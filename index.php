20a4458b1801923e8392324017dbab9018beb287
Angeles <angelessevilla18@gmail.com>
2023-03-10 21:30:56 +0100

    Add initial code base

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crud en PHP y MYSQL</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+
+<body>
+    <h1 class="text-center p-3">Hola mundo</h1>
+    <div class="container-fluid row">
+        <form class="col-4 p-3" method="POST">
+            <h3 class="text-center text-secondary">Registro de personas</h3>
+            <?php
+            include "modelo/conexion.php";
+            include "controlador/registro_persona.php";
+            ?>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Nombres</label>
+                <input type="text" class="form-control" name="nombre">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Apellidos</label>
+                <input type="text" class="form-control" name="apellido">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">DNI</label>
+                <input type="text" class="form-control" name="dni">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Fecha de nacimiento</label>
+                <input type="date" class="form-control" name="fecha">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">correo</label>
+                <input type="email" class="form-control" name="correo">
+            </div>
+            <button type="submit" class="btn btn-primary" name="btnregistrar" value="ok">Registrar</button>
+        </form>
+        <div class="col-8 p-4">
+            <table class="table">
+                <thead  class="bg-info">
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Nombres</th>
+                        <th scope="col">Apellidos</th>
+                        <th scope="col">Fecha DNI</th>
+                        <th scope="col">Fecha nacimiendo</th>
+                        <th scope="col">Correo</th>
+                        <th scope="col"></th>
+
+                        
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    include "modelo/conexion.php";
+                    $sql = $conexion->query(" select * from personas ");
+                    while($datos=$sql->fetch_object()){ ?>
+                        <tr>                        
+                        <td><?= $datos->id_personas ?></td>
+                        <td><?= $datos->nombre ?></td>
+                        <td><?= $datos->apellido ?></td>
+                        <td><?= $datos->dni ?></td>
+                        <td><?= $datos->fecha_nac ?></td>
+                        <td><?= $datos->correo ?></td>
+
+
+                        <td>
+                            <a href="" class="fs-4">📝</a>
+                            <a href="" class="fs-4">🗑️</a>
+                        </td>
+                    </tr>
+                    <?php }
+                    ?>
+                                        
+                   
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ modelo/conexion.php
@@ -0,0 +1,4 @@
+<?php
+$conexion=new mysqli("localhost", "root", "", "");
+$conexion->set_charset("utf8");
+?>
