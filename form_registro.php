429ffdee3664396b2819cd0213543bd3496f8d6b
Angeles <angelessevilla18@gmail.com>
2023-03-12 22:05:19 +0100

    Add formulario de registro

---
+++ form_registro.php
@@ -0,0 +1,30 @@
+<!--FORMULARIO -->
+
+<form class="col-4 p-3" method="POST">
+            <h3 class="text-center text-secondary">Registro de personas</h3>
+            <?php
+            include "modelo/conexion.php";
+            include "controlador/registro_persona.php";
+            ?>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Nombres</label>
+                <input type="text" class="form-control" name="nombre">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Apellidos</label>
+                <input type="text" class="form-control" name="apellido">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">DNI</label>
+                <input type="text" class="form-control" name="dni">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Fecha de nacimiento</label>
+                <input type="date" class="form-control" name="fecha">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">correo</label>
+                <input type="email" class="form-control" name="correo">
+            </div>
+            <button type="submit" class="btn btn-primary" name="btnregistrar" value="ok">Registrar</button>
+        </form>
